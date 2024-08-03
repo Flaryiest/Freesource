b1eeac645ec3cb4d1e72c174ecea5385413ec9ba
gas-propane <qxdingamon@gmail.com>
2024-08-03 09:33:58 -0600

    Update mainController.js

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -33,13 +33,11 @@ async function login(req, res, next) {
       })(req,res, next)
 }
 
-async function DistanceAndTime(origin_street, origin_city, destination_street, destination_city){
+async function findDistance(origin, destination){
     const axios = require('axios');
 
     const openCageApiKey = '0e0e78fcdfa3480885b8f9b8784c1c17';
     const openRouteServiceApiKey = '5b3ce3597851110001cf6248b38c920d855d4c37b5275e150e5d9e38';
-    const origin = origin_street + ', ' + origin_city;
-    const destination = destination_street + ', ' + destination_city;
 
     // Function to get latitude and longitude for an address
     async function geocodeAddress(address) {
@@ -59,7 +57,7 @@ async function DistanceAndTime(origin_street, origin_city, destination_street, d
     }
 
     // Function to calculate distance and time between two locations
-    async function calculateDistanceAndTime(originCoords, destinationCoords) {
+    async function calculateDistance(originCoords, destinationCoords) {
         const url = `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${openRouteServiceApiKey}&start=${originCoords.lng},${originCoords.lat}&end=${destinationCoords.lng},${destinationCoords.lat}`;
         
         try {
@@ -67,7 +65,7 @@ async function DistanceAndTime(origin_street, origin_city, destination_street, d
             if (response.data.features.length > 0) {
             const distance = response.data.features[0].properties.segments[0].distance;
             const duration = response.data.features[0].properties.segments[0].duration;
-            return { distance, duration };
+            return distance;
             } else {
             throw new Error('Distance calculation failed: No results found');
             }
@@ -84,34 +82,31 @@ async function DistanceAndTime(origin_street, origin_city, destination_street, d
         console.log(`Origin: ${originLocation.lat}, ${originLocation.lng}`);
         console.log(`Destination: ${destinationLocation.lat}, ${destinationLocation.lng}`);
 
-        const { distance, duration } = await calculateDistanceAndTime(originLocation, destinationLocation);
+        const distance = await calculateDistance(originLocation, destinationLocation);
         
         console.log(`Distance: ${(distance / 1000).toFixed(2)} km`);
-        console.log(`Duration: ${(duration / 60).toFixed(2)} minutes`);
-        return [distance, duration];
+        return distance;
     }
 
-    const values = main();
-    return values;
+    const distance = main();
+    return distance;
 }
 //INCOMPLETE FUNCTION BECAUSE IDK WHAT THE DATASET IS LIKE
 async function matchingBuyerToSeller(contractor, workers){
     let score_list = [];
     for (let i = 0; i < workers.length; i++){
-        if (contractor.city == workers[i].city){
-            const values = DistanceAndTime(contractor.street, contractor.city, workers[i].street, workers[i].city);
-            let skills_percent = 0;
-            for (let j = 0; j < contractor.tags.length; j++){
-                for (let k = 0; k < workers[i].tags.length; k++){
-                    if (contractor.tags[j] == workers[i].tags[k]){
-                        skills_percent++;
-                        break;
-                    }
+        const distance = findDistance(contractor.location, workers[i].location);
+        let skills_percent = 0;
+        for (let j = 0; j < contractor.tags.length; j++){
+            for (let k = 0; k < workers[i].tags.length; k++){
+                if (contractor.tags[j] == workers[i].tags[k]){
+                    skills_percent++;
+                    break;
                 }
             }
-            skills_percent /= (contractor.tags.length+0.5*workers[i].tags.length);
-            score_list.push([workers[i].id, 50*Math.log(16-values[0])/Math.log(16) + 25*skills_percent + 5*workers[i].rating]);
         }
+        skills_percent /= (contractor.tags.length+0.5*workers[i].tags.length);
+        score_list.push([workers[i].id, 75*Math.log(16-distance/1000)/Math.log(16) + 25*skills_percent]);
     }
     //return a sorted list of the best matched workers
     score_list.sort((a, b) => b[1] - a[1]);
@@ -121,20 +116,18 @@ async function matchingBuyerToSeller(contractor, workers){
 async function matchingSellerToBuyer(contractors, worker){
     let score_list = [];
     for (let i = 0; i < contractors.length; i++){
-        if (worker.city == contractors[i].city){
-            const values = DistanceAndTime(worker.street, worker.city, contractors[i].street, contractors[i].city);
-            let skills_percent = 0;
-            for (let j = 0; j < contractors[i].tags.length; j++){
-                for (let k = 0; k < worker.tags.length; k++){
-                    if (worker.tags[k] == contractors[i].tags[j]){
-                        skills_percent++;
-                        break;
-                    }
+        const distance = findDistance(worker.location, contractors[i].location);
+        let skills_percent = 0;
+        for (let j = 0; j < contractors[i].tags.length; j++){
+            for (let k = 0; k < worker.tags.length; k++){
+                if (worker.tags[k] == contractors[i].tags[j]){
+                    skills_percent++;
+                    break;
                 }
             }
-            skills_percent /= contractors[i].tags.length;
-            score_list.push([contractors[i].id, 50*Math.log(16-values[0])/Math.log(16) + 50*skills_percent])
         }
+        skills_percent /= contractors[i].tags.length;
+        score_list.push([contractors[i].id, 75*Math.log(16-distance/1000)/Math.log(16) + 25*skills_percent])
     }
     score_list.sort((a, b) => b[1] - a[1]);
     return score_list.slice(0, Math.min(10, score_list.length));
