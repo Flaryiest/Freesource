9718a85aa817323a616c1b0b054be0f6cef13d31
Flaryiest <ericmzuo1@gmail.com>
2024-08-02 19:34:35 -0600

    changed posts table and added date/status field

--- db/queries.js
+++ db/queries.js
@@ -7,7 +7,7 @@ async function signUp(username, password, address, email, type) {
 }
 
 async function createPost(user_id, title, price, description, tags) {
-    await pool.query("INSERT INTO posts (user_id, title, price, description, tags) VALUES (($1), ($2), ($3), ($4), ($5))", [user_id, title, price, description, tags])
+    await pool.query("INSERT INTO posts (user_id, title, price, description, tags, status) VALUES (($1), ($2), ($3), ($4), ($5), 'pending')", [user_id, title, price, description, tags])
 }
 
 async function getUserPosts(user_id) {
